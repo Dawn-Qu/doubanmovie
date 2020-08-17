a66f9a797ca8aaa05a81a8d63ee679a6a1320c05
Dawn-Qu <quchenyu2008@qq.com>
2020-08-17 08:40:40 +0800

    最新的文件

--- doubanMovie/pipelines.py
+++ doubanMovie/pipelines.py
@@ -5,12 +5,46 @@
 
 
 # useful for handling different item types with a single interface
+from scrapy.pipelines.images import ImagesPipeline
+from scrapy.http import Request
+import pymysql
 
 
 class DoubanmoviePipeline:
     def process_item(self, item, spider):
-        print('电影排名:{0}'.format(item['rank'][0]))
-        print('电影主题:{0}'.format(item['title'][0]))
-        print('电影分数:{0}'.format(item['score'][0]))
-        print('电影链接:{0}'.format(item['imgurl'][0]))
+        print('电影排名:{0}'.format(item['rank']))
+        print('电影主题:{0}'.format(item['title']))
+        print('电影分数:{0}'.format(item['score']))
+        print('电影链接:{0}'.format(item['imgurl']))
         return item
+
+
+class MyImagesPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        img_url = item['imgurl']
+        yield Request(img_url, meta={'rank': item['rank'], 'name': item['title']})
+
+    def file_path(self, request, response=None, info=None):
+        image_rank = request.meta['rank']
+        image_name = request.meta['name']
+        return '%s-%s.jpg' % (image_rank, image_name)
+
+
+class MysqlPipeline(object):
+    def open_spider(self, spider):
+        db_name = spider.settings.get('MYSQL_DB_NAME', 'douban')
+        self.db_conn = pymysql.connect('localhost', 'root', '', db_name )
+        self.db_cur = self.db_conn.cursor()
+
+    def close_spider(self, spider):
+        self.db_conn.commit()
+        self.db_conn.close()
+
+    def process_item(self, item, spider):
+        self.insert_db(item)
+        return item
+
+    def insert_db(self, item):
+        self.db_cur.execute('insert into {} (name,score,rank,img_url)values (\'{}\',{},{},\'{}\');' \
+                       .format('movie_rank',item['title'], item['score'], item['rank'], item['imgurl']))
+        self.db_conn.commit()
--- doubanMovie/settings.py
+++ doubanMovie/settings.py
@@ -12,9 +12,8 @@ BOT_NAME = 'doubanMovie'
 SPIDER_MODULES = ['doubanMovie.spiders']
 NEWSPIDER_MODULE = 'doubanMovie.spiders'
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'doubanMovie (+http://www.yourdomain.com)'
+# USER_AGENT = 'doubanMovie (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
@@ -25,66 +24,83 @@ CONCURRENT_REQUESTS = 32
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+# CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'doubanMovie.middlewares.DoubanmovieSpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 
 DOWNLOADER_MIDDLEWARES = {
-	'doubanMovie.middlewares.DoubanmovieDownloaderMiddleware': 543,
-	'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware' : None,
-	'doubanMovie.rotate_useragent.RotateUserAgentMiddleware' :400 }
+    'doubanMovie.middlewares.DoubanmovieDownloaderMiddleware': 543,
+    'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware': None,
+    'doubanMovie.rotate_useragent.RotateUserAgentMiddleware': 400}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'doubanMovie.pipelines.DoubanmoviePipeline': 300,
+    'doubanMovie.pipelines.DoubanmoviePipeline': 300,
+    'doubanMovie.pipelines.MyImagesPipeline': 1,
+    'doubanMovie.pipelines.MysqlPipeline': 300
 }
+IMAGES_STORE = r'data/images/'
+# IMAGES_URLS_FIELD = 'url'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+# AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+# AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+
+FEED_EXPORTERS = {'excel': 'doubanMovie.DataPersistence.ExcelItemExporter'}
+FEED_EXPORT_ENCODING = 'utf-8'
+
+MYSQL_DB_NAME = 'douban'
+import datetime
+
+now = datetime.datetime.now()
+logpath = 'log\\' + f'scrapy_{now.year}_{now.month}_{now.day}.log'
+
+LOG_LEVEL = 'DEBUG'
+LOG_FILE = logpath
